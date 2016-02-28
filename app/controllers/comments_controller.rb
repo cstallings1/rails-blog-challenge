8ab46dee3595194fdb96bc7accff03eaaaef5638
cstallings1 <catiestallings1@gmail.com>
2016-02-27 19:40:04 -0600

    Comment out user auth for now

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ActionController::Base
 
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  # http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
